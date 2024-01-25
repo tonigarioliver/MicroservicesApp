6d6d23ce2af89898d91ffcd352429ddcc40d4677
Antoni Gari <tonietmoscari@gmail.com>
2024-01-25 18:01:05 +0100

    Adding mqtt client service

---
+++ MqttClient/src/test/java/com/example/mqttclient/MqttClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
