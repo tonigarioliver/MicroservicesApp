5c9e4d978278860c6c39ff274c46e33c79352cf6
antoni gari <tonietmoscari@gmail.com>
2024-03-10 22:12:21 +0100

    add real time processor

---
+++ RealTimeDataService/src/main/java/com/antonigari/RealTimeDataService/RealTimeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.antonigari.RealTimeDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataServiceApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(RealTimeDataServiceApplication.class, args);
+    }
+
+}
