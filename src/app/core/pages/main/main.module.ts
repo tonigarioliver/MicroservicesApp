9b2f7f3dc9f13477774bde1e8c58be86f2ca092c
antoni gari <tonietmoscari@gmail.com>
2024-03-29 23:11:20 +0100

    Updated

--- src/app/core/pages/main/main.module.ts
+++ src/app/core/pages/main/main.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -9,7 +11,10 @@ import { MainComponent } from './main.component';
     MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class MainModule { }
--- src/app/core/pages/maintenance/maintenance.module.ts
+++ src/app/core/pages/maintenance/maintenance.module.ts
@@ -5,7 +5,7 @@ import { MaintenanceRoutingModule } from './maintenance-routing.module';
 import { MaintenanceComponent } from './maintenance.component';
 import { DeviceModelModule } from 'src/app/core/components/device-model/device-model.module';
 import { DeviceModule } from 'src/app/core/components/device/device.module';
-import { DeviceMeasurementModule } from 'src/app/core/components/device-measurement/device-measurement.module';
+import { DeviceMeasurementModule } from 'src/app/core/components/device-measurement/device-measurement/device-measurement.module';
 
 @NgModule({
   declarations: [
