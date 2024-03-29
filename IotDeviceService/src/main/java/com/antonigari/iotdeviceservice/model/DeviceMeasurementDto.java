01606343cc512bf83ecd1ecddfcb6f3a3095e881
antoni gari <tonietmoscari@gmail.com>
2024-03-29 17:18:28 +0100

    wip

--- IotDeviceService/src/main/java/com/antonigari/iotdeviceservice/model/DeviceMeasurementDto.java
+++ IotDeviceService/src/main/java/com/antonigari/iotdeviceservice/model/DeviceMeasurementDto.java
@@ -3,6 +3,12 @@ package com.antonigari.iotdeviceservice.model;
 import lombok.Builder;
 
 @Builder
-public record DeviceMeasurementDto(Long deviceMeasurementId, DeviceDto device, MeasurementTypeDto measurementType,
-                                   String topic, String name, String unit) {
+public record DeviceMeasurementDto(
+        Long deviceMeasurementId,
+        DeviceDto device,
+        MeasurementTypeDto measurementType,
+        String topic,
+        String name,
+        String unit
+) {
 }
